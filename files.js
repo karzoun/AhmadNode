d91c17e3f0b6b2517160e57d04281e9f90d969bd
“karzoun” <karzoun_27@hotmail.com>
2020-10-04 21:54:27 -0700

    added flie

---
+++ files.js
@@ -0,0 +1,9 @@
+// const fs = require('fs')
+// fs.readFile('./docs//blog.txt', (err, data) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(data.toString())
+
+// })
+// console.log('last line')
---
+++ global.js
@@ -0,0 +1,16 @@
+// const greet = (name) => {
+//     console.log(`hello, ${name}`)
+// }
+// greet("ahmad")
+// console.log(global)
+// setTimeout(() => {
+//     console.log('in the timeout')
+//     clearInterval(int)
+
+// }, 3000);
+// const int = setInterval(() => {
+//     console.log("you are awsome")
+
+// }, 1000)
+// console.log(__dirname)
+// console.log(__filename)
